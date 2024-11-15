efe76be3d298e85300be23b3a609249396fd41c6
vivek <vivan100123@gmail.com>
2024-11-16 04:17:06 +0530

    leetcode 1616 solved

--- leetcode_1616.java
+++ leetcode_1616.java
@@ -2,29 +2,34 @@ import java.util.Arrays;
 
 public class leetcode_1616 {
     public static void main(String[] args) {
-        String a = "abda";
-        String b = "acmc";
+        String a = "ulacfd";
+        String b = "jizalu";
         if(a.length()==1){
             System.out.println(true);
         }
-        StringBuilder as1 = new StringBuilder(b);
-        as1.reverse();
-//        System.out.println(a);
-//        System.out.println(as1);
-//        System.out.println(as1.charAt(as1.length()-1));
-        for(int i =0;i<a.length()-1;i++){
-            if ((a.charAt(i) == as1.charAt(i)) || (a.charAt(a.length()-1) == as1.charAt(b.length()-1))){
-                System.out.println(true);
-                break;
-            }
-            else{
-                System.out.println(false);
-                break;
-            }
+        System.out.println(checkPalindrome(a, b));
 
+    }
+    public static boolean checkPalindrome(String a, String b){
+        return checkPalin(a,b)||checkPalin(b,a);
+    }
+    private static boolean checkPalin(String a , String b){
+        int  i =0;
+        int j = a.length()-1;
+        while (i<j && a.charAt(i)==b.charAt(j)){
+            i++;
+            j--;
         }
-        System.out.println(as1.reverse());
-
-
+        return isPalindrome(a,i,j)||isPalindrome(b,i,j);
+    }
+    public static boolean isPalindrome(String a , int i , int j){
+        while (i<j){
+            if(a.charAt(i) != a.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
