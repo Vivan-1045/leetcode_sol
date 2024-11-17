090cfb4174b12fee8a12d32c68adc395816a4e9c
vivek <vivan100123@gmail.com>
2024-11-17 13:48:50 +0530

    leetcode 1813 solved

---
+++ leetcode_1813.java
@@ -0,0 +1,20 @@
+public class leetcode_1813 {
+    public static void main(String[] args) {
+        String a =  "Eating in the room";
+        String b = "Eating";
+        System.out.println(SameString(a, b));
+    }
+    public static  boolean SameString(String a, String b){
+        String[] s1 = a.split(" ");
+        String[] s2 = b.split(" ");
+        int i = 0;
+        int j = 0;
+        while (i < s1.length && i < s2.length && s1[i].equals(s2[i])) {
+            i++;
+        }
+         while (j < s1.length - i && j < s2.length - i && s1[s1.length - 1 - j].equals(s2[s2.length - 1 - j])) {
+             j++;
+         }
+         return i + j >= Math.min(s1.length, s2.length);
+    }
+}
