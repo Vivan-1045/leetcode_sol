d7c352507d2264a33eeba7e9132b91b0833ae02a
vivek <vivan100123@gmail.com>
2024-11-25 20:52:29 +0530

    leetcode 14 solved

---
+++ leetcode_14.java
@@ -0,0 +1,21 @@
+public class leetcode_14 {
+    public static void main(String[] args) {
+        String str[] = {""};
+        System.out.println(Prefix(str));
+    }
+    public static String Prefix(String str[]){
+        String pre = str[0];
+        if (str.length==0){
+            return "";
+        }
+        for (int i = 1; i< str.length;i++){
+            while (str[i].indexOf(pre)!=0){
+                if (pre.length()==0){
+                    return "";
+                }
+                pre = pre.substring(0,pre.length()-1);
+            }
+        }
+        return pre;
+    }
+}
