177c4721fc6ed80248fa5a6522d8a16d2ab0bdf0
vivek <vivan100123@gmail.com>
2024-12-25 22:15:26 +0530

    leetcode 509 solved

--- leetcode_509.java
+++ leetcode_509.java
@@ -2,12 +2,18 @@ public class leetcode_509 {
     public static void main(String[] args){
         System.out.println(fib(7));
     }
-    public static int fib(int n){
+    static long[] memo;
+    public static long fib(int n){
+        if (memo==null){
+            memo = new long[n+1];
+        }
         if (n<=1){
             return n;
         }
-        else {
-            return fib(n-1)+fib(n-2);
+        if (memo[n]!=0){
+            return memo[n];
         }
+        memo[n] = fib(n-1)+fib(n-2);
+        return memo[n];
     }
 }
