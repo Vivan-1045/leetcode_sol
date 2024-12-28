39524ae9d9dea6ee6c4ec857729f3141b63e0e8c
vivek <vivan100123@gmail.com>
2024-12-28 14:34:28 +0530

    leetcode 1346 solved

---
+++ leetcode_1346.java
@@ -0,0 +1,17 @@
+public class leetcode_1346 {
+    public static void main(String[] args){
+        int []arr = {7,1,14,11};
+        System.out.println(isDouble(arr));
+    }
+    public static boolean isDouble(int []arr){
+       int n = arr.length;
+       for (int i=0;i<n-1;i++){
+           for (int j = i+1;j<n;j++){
+               if (arr[i] == 2*arr[j] || arr[j] == 2*arr[i]){
+                   return true;
+               }
+           }
+       }
+       return false;
+    }
+}
