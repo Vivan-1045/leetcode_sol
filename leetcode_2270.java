0dfcbf2b89c4df86f832dc50465247a867990aa7
vivek <vivan100123@gmail.com>
2025-01-03 20:45:39 +0530

    leetcode 2270 solved

---
+++ leetcode_2270.java
@@ -0,0 +1,23 @@
+public class leetcode_2270 {
+    public static void main(String[] args) {
+        int [] arr = {2,3,1,0};
+        System.out.println(ValidSplit(arr));
+    }
+    public static int ValidSplit(int[] arr){
+        int n = arr.length;
+        int Split = 0;
+        long preSum[] = new long[n];
+        preSum[0] = arr[0];
+        for (int i =1;i<n;i++){
+            preSum[i] = preSum[i-1]+arr[i];
+        }
+        for (int i = 0;i<n-1;i++){
+            long left_sum = preSum[i];
+            long right_sum = preSum[n-1]-left_sum;
+            if (left_sum>=right_sum){
+                Split++;
+            }
+        }
+        return Split;
+    }
+}
