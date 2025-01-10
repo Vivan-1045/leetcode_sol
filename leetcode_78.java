77bf07a6e8984e8cc0b7dc85e74dad69efc92492
vivek <vivan100123@gmail.com>
2025-01-10 14:31:43 +0530

    leetcode 78 solved

---
+++ leetcode_78.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode_78 {
+    public static void main(String[] args) {
+        int[] arr = {1,2};
+        System.out.println(AllSubsets(arr));
+    }
+    public static List<List<Integer>> AllSubsets(int[] arr){
+        //create outer list with minimum one empty internal list
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for (int num: arr){
+            int n = outer.size();
+            for (int i =0;i<n;i++){
+                //get the copy of internal list from outer list and that internal updated list in the outer list
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+}
