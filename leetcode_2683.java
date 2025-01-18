e3db4f31ca2635de75d6f77b14548d61cedb4cd7
vivek <vivan100123@gmail.com>
2025-01-18 10:23:17 +0530

    leetcode 2683 solved

---
+++ leetcode_2683.java
@@ -0,0 +1,34 @@
+public class leetcode_2683 {
+    public static void main(String[] args) {
+        int [] A = {1,0,1,0};
+        System.out.println(isBinary(A));
+    }
+    public static boolean isBinary(int[] num){
+        int n = num.length;
+        int [] orig = new int[n];
+        orig[0]=0;
+        for (int i =0;i<n-1;i++){
+            orig[i+1] = num[i]^orig[i];
+        }
+        if ((orig[n-1]^orig[0])==num[n-1]){
+            return true;
+        }
+        orig[0]=1;
+        for (int i = 0; i < n - 1; i++) {
+            orig[i+1] = orig[i]^num[i];
+        }
+        if ((orig[n-1]^orig[0])==num[n-1]){
+            return true;
+        }
+        return false;
+    }
+
+    // Another approach
+    public static boolean isValidBinary(int[] num){
+        int res = 0;
+        for (int j : num) {
+            res ^= j;
+        }
+        return res == 0;
+    }
+}
