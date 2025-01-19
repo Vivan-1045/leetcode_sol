2ba6c2602a266fc04b57875fff3a3b0610ba2c33
vivek <vivan100123@gmail.com>
2025-01-19 09:33:48 +0530

    leetcode 01 solved

---
+++ weekly_19_01_25_01.java
@@ -0,0 +1,21 @@
+public class weekly_19_01_25_01 {
+    public static void main(String[] args) {
+      int[] arr = {3,1,1,2};
+        System.out.println(MaxSum(arr));
+    }
+    public static int MaxSum(int[]num){
+        int[] SubArray = new int[num.length];
+        for (int i = 0;i<num.length;i++){
+            int sum =0;
+            for (int j = Math.max(0, i-num[i]);j<=i;j++){
+                sum += num[j];
+                SubArray[i]=sum;
+            }
+        }
+        int Maxsum = 0;
+        for (int j : SubArray) {
+            Maxsum += j;
+        }
+        return Maxsum;
+    }
+}
