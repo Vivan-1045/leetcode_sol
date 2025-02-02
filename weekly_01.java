9d771374ab1daa7f75d7d6b42f770f717a2ceb9c
vivek <vivan100123@gmail.com>
2025-02-02 09:22:23 +0530

    solved

---
+++ weekly_01.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class weekly_01 {
+    public static void main(String[] args){
+        String s = "abcabcab";
+        System.out.println(maxDiff(s));
+    }
+    public static int maxDiff(String s){
+        int []freq = new int[26];
+        for (char ch : s.toCharArray()){
+            freq[ch-'a']++;
+        }
+
+        int mineven = Integer.MAX_VALUE;
+        int maxodd = Integer.MIN_VALUE;
+
+        for(int n : freq){
+            if (n%2 == 0 && n>0){
+                mineven = Math.min(n,mineven);
+            }
+            if(n%2 !=0 ){
+                maxodd = Math.max(n,maxodd);
+            }
+        }
+        if(mineven != Integer.MAX_VALUE && maxodd != Integer.MIN_VALUE){
+            return maxodd-mineven;
+        }
+
+        return 0;
+    }
+}
