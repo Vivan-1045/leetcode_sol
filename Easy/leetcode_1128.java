1a863400c0819afd2f879d55b725322ad5a8f624
vivek <vivan100123@gmail.com>
2025-05-04 10:30:02 +0530

    leetcode 1128 solved

---
+++ Easy/leetcode_1128.java
@@ -0,0 +1,24 @@
+package Easy;
+
+public class leetcode_1128 {
+    public static void main(String[] args) {
+        int [][] arr = {{1,2}};
+        System.out.println(equalPair(arr));
+    }
+    public static int equalPair(int[][] arr){
+        int count  =0;
+        for (int i = 0;i<arr.length-1;i++){
+            int a = arr[i][0];
+            int b = arr[i][1];
+            for (int j = i+1;j<arr.length;j++){
+                int c= arr[j][0];
+                int d= arr[j][1];
+
+                if (a==c && b==d || a==d && b==c){
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+}
