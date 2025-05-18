e99870ec4137cb41b62b628b946792e568727063
vivek <vivan100123@gmail.com>
2025-05-18 13:15:32 +0530

    leetcode 25 solved

---
+++ LinkedList/leetcode_25.java
@@ -0,0 +1,53 @@
+package LinkedList;
+
+public class leetcode_25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return kGroupReverse(head,k);
+    }
+    ListNode kGroupReverse(ListNode head , int k){
+        if (head==null|| k<=1){
+            return head;
+        }
+        int len = 0;
+        ListNode temp = head;
+        while (temp != null){
+            temp = temp.next;
+            len++;
+        }
+        ListNode prev = null;
+        ListNode current = head;
+        while (true){
+            ListNode last = prev;
+            ListNode newNode = current;
+
+            assert false;
+            ListNode next = current.next;
+            for(int i = 0;current != null && i< k;i++){
+                current.next = prev;
+                prev = current;
+                current = next ;
+                if(next != null){
+                    next = next.next;
+                }
+                len -= 1;
+                if (len<k){
+                    break;
+                }
+            }
+
+            if(last != null){
+                last.next = prev;
+            }else {
+                head = prev;
+            }
+
+            newNode.next = current;
+            if (current==null){
+                break;
+            }
+
+            prev = newNode;
+        }
+        return head;
+    }
+}
