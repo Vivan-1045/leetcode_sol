f7f03d89dee7638489e7fa4504284b015b677dc5
vivek <vivan100123@gmail.com>
2025-06-04 21:20:40 +0530

    update

--- Maths/leetcode_1492.java
+++ Maths/leetcode_1492.java
@@ -1,8 +1,34 @@
 package Maths;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class leetcode_1492 {
     public static void main(String[] args) {
-
+        System.out.println(kth(4,2));
+    }
+    public static int kth(int n,int k){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1;i<=n;i++){
+            if(n%i==0){
+                list.add(i);
+            }
+            if(list.size()==k){
+                return list.get(k-1);
+            }
+        }
+        return -1;
     }
 
+    public static int kth1(int n, int k){
+        for(int i = 1;i<=n;i++){
+            if(n%i==0){
+                k--;
+            }
+            if(k==0){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
