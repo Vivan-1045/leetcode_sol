914d678c388fec4e81322f38ad65cecd9ab5c577
vivek <vivan100123@gmail.com>
2025-07-02 18:46:56 +0530

    leetcode 199 update

--- BinaryTree/leetcode_199.java
+++ BinaryTree/leetcode_199.java
@@ -5,19 +5,21 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
 public class leetcode_199 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public List<Integer> rightSideView(TreeNode root) {
         return rightView(root);
     }
