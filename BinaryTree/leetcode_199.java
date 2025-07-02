1f81cd135454f40a8ef4620f1593b89b7efd8f41
vivek <vivan100123@gmail.com>
2025-07-02 18:22:33 +0530

    leetcode 199 solved

---
+++ BinaryTree/leetcode_199.java
@@ -0,0 +1,50 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+public class leetcode_199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        return rightView(root);
+    }
+    List<Integer> rightView(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+
+        while(!que.isEmpty()){
+            int queSize = que.size();
+            for(int i = 0;i<queSize;i++){
+                TreeNode currNode = que.poll();
+                if(i==queSize-1){
+                    ans.add(currNode.val);
+                }
+                if(currNode.left != null){
+                    que.offer(currNode.left);
+                }
+                if(currNode.right != null){
+                    que.offer(currNode.right);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
