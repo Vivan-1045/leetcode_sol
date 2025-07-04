bceb8cf0de03fb7ca5fea061cbc0c4fd195f3aee
vivek <vivan100123@gmail.com>
2025-07-04 17:16:17 +0530

    leetcode 543 solved

---
+++ BinaryTree/leetcode_543.java
@@ -0,0 +1,37 @@
+package BinaryTree;
+
+public class leetcode_543 {
+    class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+   }
+
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter-1;
+    }
+
+    int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+
+        int leftH = height(root.left);
+        int rightH = height(root.right);
+
+        int dia = leftH + rightH + 1;
+        diameter = Math.max(dia,diameter);
+
+        return Math.max(leftH,rightH)+1;
+    }
+
+}
