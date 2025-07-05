ad9fe4f0c9e67fe30173f8ab52408aae3794d483
vivek <vivan100123@gmail.com>
2025-07-05 14:19:32 +0530

    leetcode 443 solved

---
+++ Two_Pointer/leetcode_443.java
@@ -0,0 +1,39 @@
+package Two_Pointer;
+
+import java.util.Arrays;
+
+public class leetcode_443 {
+    public static void main(String[] args) {
+        char[] arr = {'a','a','b','b','b','b','b','b','b','b','b','b','b','b'};
+        System.out.println(compressedLength(arr));
+    }
+    public static int compressedLength(char [] arr){
+
+        int write = 0;
+        int i = 0;
+
+        while (i < arr.length) {
+            char currentChar = arr[i];
+            int count = 0;
+            int j = i;
+
+            while (j < arr.length && arr[j] == currentChar) {
+                count++;
+                j++;
+            }
+
+            arr[write++] = currentChar;
+
+            if (count > 1) {
+                String countStr = Integer.toString(count);
+                for (int k = 0; k < countStr.length(); k++) {
+                    arr[write++] = countStr.charAt(k);
+                }
+            }
+
+            i = j;
+        }
+
+        return write;
+    }
+}
