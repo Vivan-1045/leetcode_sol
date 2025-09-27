75dd4f4ccf833567056627f0b029413ebd57a5ff
vivek <vivan100123@gmail.com>
2025-09-27 20:59:40 +0530

    biweekly 166 attended

---
+++ Contests/biweekly_166.java
@@ -0,0 +1,27 @@
+package Contests;
+
+import java.util.Arrays;
+
+public class biweekly_166 {
+    public int climbStairs(int n, int[] costs) {
+        return minCost(n,costs);
+    }
+    int minCost(int n, int[] arr){
+        int dp[] = new int[n+1];
+
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i =0;i<=n;i++){
+            for(int j = 1;j<=3;j++){
+                int nextS = i+j;
+                if(nextS<=n){
+                    int cost = arr[nextS-1]+j*j;
+                    dp[nextS] = Math.min(dp[nextS],dp[i]+cost);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
