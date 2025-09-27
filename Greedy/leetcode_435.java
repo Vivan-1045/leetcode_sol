bc7ba2bf37a4ed3a3cd06a130a708c1b7e2aa065
vivek <vivan100123@gmail.com>
2025-09-27 14:41:28 +0530

    leetcode 435 solved

---
+++ Greedy/leetcode_435.java
@@ -0,0 +1,26 @@
+package Greedy;
+
+public class leetcode_435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        return totalOverlapInt(intervals);
+    }
+
+    int totalOverlapInt(int[][] intV){
+        Arrays.sort(intV,(a,b)->Integer.compare(a[1],b[1]));
+
+        int initE = intV[0][1];
+        int cnt = 0;
+        for(int i = 1;i<intV.length;i++){
+            int s = intV[i][0],e=intV[i][1];
+
+            if(initE > s){
+                cnt++;
+            }
+            else if(initE <= s){
+                initE = e;
+            }
+        }
+
+        return cnt;
+    }
+}
