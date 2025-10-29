a06ab6f6f2c0a127c9b9c689549a8e173fae00e7
vivek <vivan100123@gmail.com>
2025-10-30 00:20:50 +0530

    leetcode 538 solved

---
+++ BinaryTree/DFS/leetcode_538.java
@@ -0,0 +1,37 @@
+package BinaryTree.DFS;
+
+public class leetcode_538 {
+    class TreeNode{
+        int val;
+        TreeNode left,right;
+
+        TreeNode(int val, TreeNode l, TreeNode r){
+            this.val = val;
+            this.left = l;
+            this.right = r;
+        }
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        solve(root);
+        return root;
+    }
+    int sum = 0;
+
+    void solve(TreeNode root){
+        if(root==null){
+            return;
+        }
+
+        if(root.left==null && root.right == null){
+            sum += root.val;
+            root.val = sum;
+            return;
+        }
+
+        solve(root.right);
+        sum += root.val;
+        root.val = sum;
+        solve(root.left);
+    }
+}
